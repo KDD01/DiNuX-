498277de901af8fbdd04a9343d1c2ebcbc8b9847
Dinush Dilhara <dinushdilhara72@gmail.com>
2026-05-07 22:30:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -160,7 +160,7 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-if prompt := st.chat_input("ඔයාගේ හිතේ තියෙන දේ කියන්න... ❤️"):
+if prompt := st.chat_input("Ask anything"):
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
